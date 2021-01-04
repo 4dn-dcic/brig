02dcccfca107a0454bba077273548f455273f92d
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-01-04 10:40:58 -0500

    Suppress the echoevent debugging functionality now that things are again debugged.

--- functions/4dn-status/src/lambda_function.py
+++ functions/4dn-status/src/lambda_function.py
@@ -201,19 +201,19 @@ def lambda_handler(event, context):
     # It might be a security problem to leave this turned on in production, but it may be useful to enable
     # this during development to be able to see what's coming through. -kmp 7-Jul-2020
     # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    if params.get("echoevent"):
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Content-Type": "application/json",
-                "Cache-Control": "public, max-age=120",
-                # Note that this does not do Access-Control-Allow-Origin, etc.
-                # as this is for debugging only. -kmp 19-Mar-2020
-            },
-            "body": json.dumps(event, indent=2),
-            # Maybe also this, too ...
-            # "context": json.dumps(context)  # or repr(context)
-        }
+    # if params.get("echoevent"):
+    #     return {
+    #         "statusCode": 200,
+    #         "headers": {
+    #             "Content-Type": "application/json",
+    #             "Cache-Control": "public, max-age=120",
+    #             # Note that this does not do Access-Control-Allow-Origin, etc.
+    #             # as this is for debugging only. -kmp 19-Mar-2020
+    #         },
+    #         "body": json.dumps(event, indent=2),
+    #         # Maybe also this, too ...
+    #         # "context": json.dumps(context)  # or repr(context)
+    #     }
     # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
     # The referer is available in a standard event packaging, in the headers,
